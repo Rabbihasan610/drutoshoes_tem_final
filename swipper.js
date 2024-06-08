7c5ce99a03fb7844407432d3a837a9c5c9a717bc
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-06-08 18:57:32 +0600

    update just landed page

--- swipper.js
+++ swipper.js
@@ -1,3 +1,5 @@
+// header card dropdown menu js
+
 document.addEventListener("DOMContentLoaded", function () {
     const cartIcon = document.querySelector(".wrapper-top-cart");
     const dropdownCart = document.getElementById("dropdown-cart");
@@ -11,8 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
-
+// banner swipper page js
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
@@ -31,3 +32,67 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
 });
+
+
+// slick slider just landed page 
+
+$(document).ready(function () {
+    $(".slider").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: false,
+        prevArrow: '<button class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button class="slick-next"><i class="fa fa-angle-right"></i></button>',
+        dots: false,
+        centerMode: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3, // Adjust to show 3 items on even smaller screens if needed
+                }
+            },
+        ]
+    });
+});
+
+
+// just landed page js card 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const casualsBtn = document.getElementById('btn-casuals');
+    const sneakersBtn = document.getElementById('btn-sneakers');
+    const sandalsBtn = document.getElementById('btn-sandals');
+
+    const casualsSlider = document.getElementById('slider-casuals');
+    const sneakersSlider = document.getElementById('slider-sneakers');
+    const sandalsSlider = document.getElementById('slider-sandals');
+
+    function showSlider(activeBtn, activeSlider) {
+        [casualsBtn, sneakersBtn, sandalsBtn].forEach(btn => btn.classList.remove('active'));
+        activeBtn.classList.add('active');
+
+        [casualsSlider, sneakersSlider, sandalsSlider].forEach(slider => slider.classList.add('hidden'));
+        activeSlider.classList.remove('hidden');
+    }
+
+    casualsBtn.addEventListener('click', function () {
+        showSlider(casualsBtn, casualsSlider);
+    });
+
+    sneakersBtn.addEventListener('click', function () {
+        showSlider(sneakersBtn, sneakersSlider);
+    });
+
+    sandalsBtn.addEventListener('click', function () {
+        showSlider(sandalsBtn, sandalsSlider);
+    });
+});
+
